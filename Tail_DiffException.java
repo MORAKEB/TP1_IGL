6bae803a059521eeab45ff5d4448caefd7c91a1b
Souad360 <32779871+Souad360@users.noreply.github.com>
2017-10-22 23:23:51 +0200

    Generation des exceptions
    lance une exception si les tableaux sont de taille différentes

---
+++ Tail_DiffException.java
@@ -0,0 +1,15 @@
+
+public class Tail_DiffException extends Exception {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public void tailleDiff(){
+
+        System.out.println("la taille des deux tableaux est différente  , on peux pas les sommer !!!! ");
+    }
+
+
+}
